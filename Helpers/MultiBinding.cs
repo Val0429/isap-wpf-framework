a05f9a460aa6af5c530a3d9f1320537c2447b817
Val <val.liu@isapsolution.com>
2019-09-09 15:34:17 +0800

    add helper - multi binding

---
+++ Helpers/MultiBinding.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Library.Helpers {
+    public class MultiBinding : System.Windows.Data.MultiBinding {
+        public MultiBinding(BindingBase b1) {
+            Bindings.Add(b1);
+        }
+        public MultiBinding(BindingBase b1, BindingBase b2) {
+            Bindings.Add(b1);
+            Bindings.Add(b2);
+        }
+        public MultiBinding(BindingBase b1, BindingBase b2, BindingBase b3) {
+            Bindings.Add(b1);
+            Bindings.Add(b2);
+            Bindings.Add(b3);
+        }
+    }
+}
