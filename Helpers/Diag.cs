0c2ffad2f3c99a814202470279ab264a6cea6cbe
Val <val.liu@isapsolution.com>
2019-09-11 11:33:01 +0800

    add Diag

---
+++ Helpers/Diag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Helpers {
+
+    public static class Diag {
+        public static void SetApply(object element, bool apply) {
+            PresentationTraceSources.SetTraceLevel(element, PresentationTraceLevel.High);
+        }
+    }
+}
